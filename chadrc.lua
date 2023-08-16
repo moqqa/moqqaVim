7864e8b8367c362edd63868eccb95d5fe8b158c4
Mocha <71618230+Mochaccinoo@users.noreply.github.com>
2023-08-16 00:09:10 -0400

    trying theme out

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "catppuccin",
-  theme_toggle = { "catppuccin", "ayu_light" },
+  theme = "kanagawa",
+  theme_toggle = { "kanagawa", "ayu_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
