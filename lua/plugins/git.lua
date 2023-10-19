d635d2ed93c43b85382fb1ce872a86617374078a
Mocha <LegalMustela@gmail.com>
2023-10-19 14:16:30 -0400

    💤💤 lazyvim 0.10 💤💤

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -8,4 +8,23 @@ return {{
         desc = "Neogit"
     }},
     opts = {}
-}}
+},
+{
+    "nvim-neorg/neorg",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    build = ":Neorg sync-parsers",
+    opts = {
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {}, -- icons
+        ["core.dirman"] = { -- workspace management
+          config = {
+            workspaces = {
+              notes = "~/Notes",
+            },
+          },
+        },
+        ["core.export"] = {}, -- export to markdown
+      },
+    },}
+}
