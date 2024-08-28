6460c92a5195b2f0a147a1c1411a5d2400e18fce
Mocha <LegalMustela@gmail.com>
2024-08-28 13:34:54 -0400

    fix: telescope to fzf

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -8,27 +8,27 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-    spec = {{
-        "LazyVim/LazyVim",
-        import = "lazyvim.plugins"
-    }, {
-        import = "plugins"
-    }},
-    defaults = {
-        -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-        -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-        lazy = false,
-        -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-        -- have outdated releases, which may break your Neovim install.
-        version = true -- always use the latest git commit
+  spec = { {
+    "LazyVim/LazyVim",
+    import = "lazyvim.plugins",
+  }, {
+    import = "plugins",
+  } },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+  },
+  checker = {
+    enabled = true,
+  }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = { "gzip", "matchit", "matchparen", "netrwPlugin", "tarPlugin", "tohtml", "zipPlugin" },
     },
-    checker = {
-        enabled = true
-    }, -- automatically check for plugin updates
-    performance = {
-        rtp = {
-            -- disable some rtp plugins
-            disabled_plugins = {"gzip", "matchit", "matchparen", "netrwPlugin", "tarPlugin", "tohtml", "zipPlugin"}
-        }
-    }
+  },
 })
