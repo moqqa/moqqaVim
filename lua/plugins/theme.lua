07b681f23637f6c0ad00e992bda0f0f984815537
Mocha <LegalMustela@gmail.com>
2024-11-19 15:16:46 -0500

    feat (theme): change to oxocarbon
    light mode is now possible

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -100,6 +100,13 @@ return {
   },
   {
     "nyoom-engineering/oxocarbon.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    config = function()
+      vim.opt.background = "dark"
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end,
   },
   -- {
   --   "xiyaowong/transparent.nvim",
